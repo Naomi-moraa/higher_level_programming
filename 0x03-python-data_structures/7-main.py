44587f1af286cfcdbe988c2e7398cab9f7606d2b
Naomi-moraa <naomimoraa565@gmail.com>
2025-07-29 12:28:51 +0300

    8-multiple_returns.py

--- 0x03-python-data_structures/7-main.py
+++ 0x03-python-data_structures/7-main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 add_tuple = __import__('7-add_tuple').add_tuple
 
-print(add_tuple((1, 2), (3, 4)))       # (4, 6)
-print(add_tuple((1, ), (3, 4)))        # (4, 4)
-print(add_tuple((), (5,)))             # (5, 0)
-print(add_tuple((7, 8, 9), (1, 2)))    # (8, 10)
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)                     
+print(add_tuple(tuple_a, (1, )))      
+print(add_tuple(tuple_a, ()))         
